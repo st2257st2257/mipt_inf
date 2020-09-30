d12e9563961b4380e3c275f6d0c140c53ddb6377
st2257st2257 <68289534+st2257st2257@users.noreply.github.com>
2020-09-30 13:00:12 +0300

    Update img.py

--- img.py
+++ img.py
@@ -30,11 +30,8 @@ clock = pygame.time.Clock()
 
 
 
-#             0           1           2           3         4
-color = [(15,82,14),(234,46,67),(146,106,14),(180,97,16),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0)]
-
-
-
+#             0           1           2           3           4            5          6             7           8 
+color = [(15,82,14),(234,46,67),(146,106,14),(180,97,16),(14,147,145),(255,0,0),(153,227,237),(14,147,37),(249,194,194),(0,0,0)]
 
 
 #create sun
@@ -73,30 +70,22 @@ while not done:
     # Draw on the screen a GREEN line from (0, 0) to (50, 30)
     # 5 pixels wide.
     pygame.draw.line(screen, GREEN, [0, 0], [50,30], 5)
-
     # Draw on the screen 3 BLACK lines, each 5 pixels wide.
     # The 'False' means the first and last points are not connected.
     pygame.draw.lines(screen, BLACK, False, [[0, 80], [50, 90], [200, 80], [220, 30]], 5)
-
     # Draw on the screen a GREEN line from (0, 50) to (50, 80)
     # Because it is an antialiased line, it is 1 pixel wide.
     pygame.draw.aaline(screen, GREEN, [0, 50],[50, 80], True)
-
     # Draw a rectangle outline
     pygame.draw.rect(screen, BLACK, [75, 10, 50, 20], 2)
-
     # Draw a solid rectangle
     pygame.draw.rect(screen, BLACK, [150, 10, 50, 20])
-
  # Draw an ellipse outline, using a rectangle as the outside boundaries
     pygame.draw.ellipse(screen, RED, [225, 10, 50, 20], 2)
-
     # Draw an solid ellipse, using a rectangle as the outside boundaries
     pygame.draw.ellipse(screen, RED, [300, 10, 50, 20])
-
     # This draws a triangle using the polygon command
     pygame.draw.polygon(screen, BLACK, [[100, 100], [0, 200], [200, 200]], 5)
-
     # Draw an arc as part of an ellipse.
     # Use radians to determine what angle to draw.
     pygame.draw.arc(screen, BLACK,[210, 75, 150, 125], 0, pi/2, 2)
@@ -110,23 +99,23 @@ while not done:
     pygame.draw.rect(screen, color[7], [0, 150, 400, 300])
 
     #tree
-    pygame.draw.line(screen, color[8], [6.9 * 40, 3.2 * 40], [6.9 * 40, 4.8 * 40], 12)
-    pygame.draw.circle(screen, (0, 0, 0), [7*40, 150-20], 20);        pygame.draw.circle(screen, color[0], [7*40, 150-20], 20);
+    pygame.draw.line(screen, color[9], [6.9 * 40, 3.2 * 40], [6.9 * 40, 4.8 * 40], 12)
+    pygame.draw.circle(screen, color[0], [7*40, 150-20], 20);        pygame.draw.circle(screen, color[0], [7*40, 150-20], 20);
     pygame.draw.circle(screen, (0, 0, 0), [7*40+5, 150-20+5], 20);    pygame.draw.circle(screen, color[0], [7*40+5, 150-20+5], 20);
-    pygame.draw.circle(screen, (0, 0, 0), [7*40+5, 150-20-5], 20);    pygame.draw.circle(screen, color[0], [7*40+5, 150-20+5], 20);
-    pygame.draw.circle(screen, (0, 0, 0), [7*40-5, 150-20+5], 20);    pygame.draw.circle(screen, color[0], [7*40+5, 150-20+5], 20);
-    pygame.draw.circle(screen, (0, 0, 0), [7*40-5, 150-20-5], 20);    pygame.draw.circle(screen, color[0], [7*40+5, 150-20+5], 20);
+    pygame.draw.circle(screen, color[0], [7*40+5, 150-20-5], 20);    pygame.draw.circle(screen, color[0], [7*40+5, 150-20+5], 20);
+    pygame.draw.circle(screen, (0, 255, 0), [7*40-5, 150-20+5], 20);    pygame.draw.circle(screen, color[0], [7*40+5, 150-20+5], 20);
+    pygame.draw.circle(screen, (0, 55, 0), [7*40-5, 150-20-5], 20);    pygame.draw.circle(screen, color[0], [7*40+5, 150-20+5], 20);
 
     #house
-    pygame.draw.polygon(screen, color[5], [[0.4 * 40, 3.1 * 40], [(0.4 * 40 + 2.9 * 40) / 2 , 3.1 * 40 - 30],[0.4 * 40 + 2.9 * 40, 3.1 * 40]], 30)
+    pygame.draw.polygon(screen, color[5], [[0.4 * 40, 3.1 * 40], [(0.4 * 40 + 2.9 * 40) / 2 , 3.1 * 40 - 30],[0.4 * 40 + 2.9 * 40, 3.1 * 40]], 30)    
     pygame.draw.rect(screen, color[2], [0.4*40, 3.1*40-13, 2.9*40+1, 2.1*40])
     pygame.draw.rect(screen, color[3], [ 1.4*40-1 , 3.6*40 -1 , 0.8*40+2, 0.7*40 +2])
     pygame.draw.rect(screen, color[4], [ 1.4*40 , 3.6*40  , 0.8*40, 0.7*40 ])
 
 
     #sun
-    pygame.draw.polygon(screen, WHITE, c,1 )
-    pygame.draw.circle(screen, color[1] , [85*4, 40], 20)
+    pygame.draw.polygon(screen,color[8], c,1 )
+    pygame.draw.circle(screen, color[8] , [85*4, 40], 20)
 
     # cloud
     pygame.draw.circle(screen, (0, 0, 0),[50 * 4, 30], 21);  pygame.draw.circle(screen, (255, 255, 255),[50 * 4, 30], 20);
@@ -157,8 +146,6 @@ while not done:
     
     # Draw a circle
     pygame.draw.circle(screen, (255,255,0) , [150, 150], 100)
-
-
     pygame.draw.circle(screen, (255, 0, 0), [150 - 26, 150-20], 20)
     pygame.draw.circle(screen, (255, 0, 0), [150 + 28, 150-20], 20)
     pygame.draw.circle(screen, (0, 0, 0), [150 - 26, 150-20], 8)
@@ -166,7 +153,6 @@ while not done:
     pygame.draw.rect(screen, BLACK, [150-45, 150+30, 90, 15])
     pygame.draw.line(screen, BLACK, [150 - 20, 150-20-20], [150 - 26-40, 150-20-20-30], 5)
     pygame.draw.line(screen, BLACK, [150 + 22, 150-20-20], [150 + 26+40, 150-20-20-30], 5)
-
     # Go ahead and update the screen with what we've drawn.
     # This MUST happen after all the other drawing commands."""
     pygame.display.flip()
