b908111540a9d61b39a4c94f40cc6fd6b5ebcd42
st2257st2257 <68289534+st2257st2257@users.noreply.github.com>
2020-10-02 02:31:29 +0300

    Update task11.py

--- lab2/task11.py
+++ lab2/task11.py
@@ -1,3 +1,4 @@
+  
 import numpy as np
 
 import turtle
@@ -6,11 +7,11 @@ turtle.speed(100)
 
 def task5(n):
     for i in range(1, n+1):
-        for j in range(360):
+        for j in range(int(360/i)):
             turtle.forward(2)
             turtle.left(i)
         turtle.left(180)
-        for j in range(360):
+        for j in range(int(360/i)):
             turtle.forward(2)
             turtle.left(i)
 
