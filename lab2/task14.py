dd4df4391d0effcba48d6cb1a6d544fb3003215d
st2257st2257 <68289534+st2257st2257@users.noreply.github.com>
2020-10-02 02:46:48 +0300

    Update task14.py

--- lab2/task14.py
+++ lab2/task14.py
@@ -7,14 +7,25 @@ turtle.speed(10)
 turtle.left(90)
 
 
-def smile(n):
+def smile(n,s=1):
     #face_big
     b = 180 - 180*(n-2)/n
     a = 180 - 2*b
 
     for i in range(n):
         turtle.forward(100)
-        turtle.right(180 - a)
+        if s>0:
+            turtle.right(180 - a)
+        else:
+            turtle.left(180 - a)
+    
+    if (n%2==0):
+        turtle.forward(33)
+        turtle.left(a)
+        turtle.forward(33)
+        turtle.right(2*b)        
+        #turtle.forward(33)
+        smile(n//2,-s)
 
 
 
