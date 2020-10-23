6f4466823d53fcfffd21e5f9be929d38515f4f5a
st2257st2257 <68289534+st2257st2257@users.noreply.github.com>
2020-10-23 11:14:54 +0300

    Update lab8.py

--- lab6/lab8.py
+++ lab6/lab8.py
@@ -46,23 +46,25 @@ class ball():
         )
 
     def move(self):
-	self.vy -= grav
+        self.vy -= grav
+
+        
+
+        """
+        	if ((self.x > (screen_w - self.vx))|(self.x < self.vx)):
+        	self.vx *= -0.75
+        if ((self.y > (screen_h - self.vy))|(self.y < self.vy)):
+        	self.vy *= -0.75
+
+        """
+
+        if ((self.x > (screen_w))|(self.x < 0)):
+            self.vx *= -0.75
+        if ((self.y > (screen_h))|(self.y < 0)):
+            self.vy *= -0.75
 	
-	"""
-		if ((self.x > (screen_w - self.vx))|(self.x < self.vx)):
-		self.vx *= -0.75
-	if ((self.y > (screen_h - self.vy))|(self.y < self.vy)):
-		self.vy *= -0.75
-	"""
-	if ((self.x > (screen_w))|(self.x < 0)):
-		self.vx *= -0.75
-	if ((self.y > (screen_h))|(self.y < 0)):
-		self.vy *= -0.75
-	
-	
-		
-	self.x += self.vx
-	self.y += self.vy
+        self.x += self.vx
+        self.y += self.vy
         
 
     def hittest(self, obj):
